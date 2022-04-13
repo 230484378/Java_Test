2ca4c9d79cc46f02ce9fdaa0e4ec82ad5b8dc55c
贺存茂 <2304084378@qq.com>
2022-04-14 00:18:44 +0800

    Init_4

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,6 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("asdad");
-        System.out.println("Test");
+        System.out.println("Test_2");
     }
 }
